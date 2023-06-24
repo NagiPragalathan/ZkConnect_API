65d95d7afa03b1873bc1df850c88df9b0fbda76e
nagipragalathan <kk2069037@gmail.com>
2023-06-25 00:13:55 +0530

    finnal signup page

--- backend/views.py
+++ backend/views.py
@@ -8,8 +8,6 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import users
 
-from django.db import transaction
-
 @api_view()
 def home(request):
     return Response({
@@ -23,20 +21,19 @@ def signup(request):
     password = request.data.get('password')
     email = request.data.get('email')
     role = request.data.get('role')
-    print(username,password,email,role)
     if username and password and email:
-        print(f"The {username} are user creation started")
-        print(username,password,email,role)
-        # user = User.objects.create_user(username=username, password=password, email=email)
-        # print("will print")
-        # print(user.id)
-        with transaction.atomic():
-            obj = users(userid=len(users.objects.all()),username=username,email=email,password=password,role=role)
+        try:
+            print(f"The {username} are user creation started")
+            print(username,password,email,role)
+            user = User.objects.create_user(username=username, password=password, email=email)
+            print("will print")
+            print(user.id)
+            obj = users(userid=user.id,username=username,email=email,password=password,role=role)
             obj.save()
             print("user created")
-        return Response({'sucess': 'User Created'},status=status.HTTP_201_CREATED)
-        # except:
-        #     return Response({'error': 'Unable to create user.'}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({'Susses': 'User Created'},status=status.HTTP_201_CREATED)
+        except Exception as e:
+            return Response({'error': 'Unable to create user.'+str(e)}, status=status.HTTP_400_BAD_REQUEST)
     else:
         return Response({'error': 'Missing required fields.'}, status=status.HTTP_400_BAD_REQUEST)
 
