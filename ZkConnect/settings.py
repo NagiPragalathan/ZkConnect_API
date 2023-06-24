d0840335baa15a6957f34e6e1d04f70b787196f8
nagipragalathan <kk2069037@gmail.com>
2023-06-24 23:14:19 +0530

    sample

--- ZkConnect/settings.py
+++ ZkConnect/settings.py
@@ -15,11 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-import dns.resolver
-
-dns.resolver.default_resolver=dns.resolver.Resolver(configure=False)
-dns.resolver.default_resolver.nameservers=['8.8.8.8']
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
--- backend/views.py
+++ backend/views.py
@@ -7,9 +7,6 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import users
-from django.contrib.auth import authenticate, login
-from django.shortcuts import render, redirect
-from django.contrib.auth import logout
 
 @api_view()
 def home(request):
@@ -17,42 +14,42 @@ def home(request):
         'status' : 200,
         'message' : 'Yes! Django rest FrameWork is Working !!!'
     })
-    
-@api_view(['POST','PATCH'])
+
+@api_view(['POST'])
 def signup(request):
     username = request.data.get('username')
     password = request.data.get('password')
     email = request.data.get('email')
     role = request.data.get('role')
-    # user = User.objects.create_user(username=username, email=email, password=password)
-    # print("User created with username:", username)
-    obj = users(userid=22, username=username, email=email, password=password, role=role)
-    obj.save()
-    print("User details saved in the database")
-    return Response({'Success': 'User Created'}, status=status.HTTP_201_CREATED)
-   
+    if username and password and email:
+        print(f"The {username} are user creation started")
+        print(username,password,email,role)
+        user = User.objects.create_user(username=username, password=password, email=email)
+        print("will print")
+        print(user.id)
+        obj = users(userid=user.id,username=username,email=email,password=password,role=role)
+        obj.save()
+        print("user created")
+        return Response({'sucess': 'User Created'},status=status.HTTP_201_CREATED)
+        # except:
+        #     return Response({'error': 'Unable to create user.'}, status=status.HTTP_400_BAD_REQUEST)
+    else:
+        return Response({'error': 'Missing required fields.'}, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['POST'])
 def login(request):
     username = request.data.get('username')
     password = request.data.get('password')
-    user = authenticate(request, username=username, password=password)
+
     if username and password:
-        if user is not None:
-            login(request, user)
-            # Redirect to a success page or any other desired action
-            return Response({'Susses': 'Invalid username or password.'}, status=status.HTTP_200_OK)
+        user = User.objects.filter(username=username).first()
+        if user and user.check_password(password):
+            # Perform any additional operations or authentication checks here if needed
+            return Response(status=status.HTTP_200_OK)
         else:
             return Response({'error': 'Invalid username or password.'}, status=status.HTTP_401_UNAUTHORIZED)
     else:
         return Response({'error': 'Missing required fields.'}, status=status.HTTP_400_BAD_REQUEST)
-
-@api_view(['POST',])
-def logout_view(request):
-    logout(request)
-    # Redirect to a desired page after logging out
-    return redirect('home')
-
 
 def welcome_home(request):
     return render(request,'welcome_page.html')
