7692d7a062a4d902bf994f19a6004ef7120163ae
nagipragalathan <kk2069037@gmail.com>
2023-06-25 14:31:59 +0530

    error solve

--- test/ref_toc.py
+++ test/ref_toc.py
@@ -5,10 +5,7 @@ def refresh_access_token():
     refresh_url = 'https://oauth2.googleapis.com/token'
 
     refresh_data = {
-        'client_id': '738289713788-oaob3rgnc39pt2sa2d3gcs7hv36vua9c.apps.googleusercontent.com',
-        'client_secret': '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',
-        'refresh_token': "1//04vDWzDauNkaxCgYIARAAGAQSNwF-L9IrbyaVngBdgOS-e2VlAXb1B3QpTZUQy8ywMjCBoEDMuPtXipTjhULtV9p_tFrwdbtak8g",
-        'grant_type': 'refresh_token'
+
     }
 
     # Send the request to refresh the access token
